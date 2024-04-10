2f8feaf610ede3b19b20c8f2f5a798ef4374425b
Paul Dawidowicz <nkipa2057@gmail.com>
2024-04-10 14:47:45 +0700

    Update 3_💡Suggest.py

--- 3_💡Suggest.py
+++ 3_💡Suggest.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
-from streamlit_option_menu import option_menu
+
 
 st.markdown("<h1 style='text-align: center; color: Black;'>NutrAGI </br> Nutritional Assistant</h1>", unsafe_allow_html=True)
 
